d6eebb1b40b46f3063e90507ac1354e0506dc2f6
Dennis Ocana <dennisocana@gmail.com>
2024-11-20 19:17:47 -0500

    formatted a document

--- tareas/app/Models/Tarea.php
+++ tareas/app/Models/Tarea.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tarea extends Model
 {
-    /** @use HasFactory<\Database\Factories\TareaFactory> */
+    /** 
+     * 
+     * @use HasFactory<\Database\Factories\TareaFactory> */
     use HasFactory;
+
     protected $fillable = [
         'title',
         'description',
